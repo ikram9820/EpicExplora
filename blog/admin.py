9325c763e35fcb47f443cb76f60d053245f8234a
ikram9820 <ikram9820khan@gmail.com>
2023-08-08 18:35:18 +0500

    implementing the admin of blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','status','publish',"body",'author']
+    list_filter = ["status","publish","author"]
+    search_fields= ["title","body"]
+    prepopulated_fields = {"slug":("title",)}
+    list_editable = ["status"]
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
