be8f4033f9c6931d03328ef6acacdb10c0f2f54b
ikram9820 <ikram9820khan@gmail.com>
2023-08-09 21:44:40 +0500

    sending email to console with smtp

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import post_detail,post_list,share_post
 
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('<int:id>/',share_post, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
 from .models import Post
 from .forms import EmailPostForm
@@ -29,12 +30,19 @@ def post_detail(request,year,month,day,post):
                              publish__day=day,status=Post.Status.PUBLISHED)
     return render(request,"blog/post/detail.html",{'post':post}) 
 
-def share_post(request,pk):
-    post = get_object_or_404(Post, pk=pk)
+def share_post(request,id):
+    post = get_object_or_404(Post, pk=id)
+    sent = False
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you to read {post.title}"
+            message = f"read {post.title} at {post_url} \n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject,message,"ikram9820khan@gmail.com",[cd["to"]])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, "blog/post/share.html",{"form": form,"post":post})
+    return render(request, "blog/post/share.html",{"form": form,"post":post,"sent":sent})
