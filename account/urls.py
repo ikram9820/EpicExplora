da7d7bbffb9fd8aeaf7040a56170e5b3686e6f07
ikram9820 <ikram9820khan@gmail.com>
2023-08-10 23:09:16 +0500

    implementing user registration feature

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 #  path('login/', views.user_login, name='login'),
  path('', views.dashboard, name='dashboard'),
  path('', include('django.contrib.auth.urls')),
+  path('register/', views.register, name='register'),
+
 #  path('login/', auth_views.LoginView.as_view(), name='login'),
 #  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 #  path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
